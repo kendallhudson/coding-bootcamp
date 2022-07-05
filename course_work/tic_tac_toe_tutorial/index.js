f3b614394ec8d8fc3b07224feed0600cf676d591
Kendall Hudson <64994077+kendallhudson@users.noreply.github.com>
2022-07-05 18:47:20 -0500

    Tic Tac Toe Tutorial (Arcade Resubmission)

---
+++ course_work/tic_tac_toe_tutorial/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Board from "./components/Board"
+import App from "./App";
+// const App = () => {
+//     return <div className="App">
+//       <Game />
+//       <Square />
+//       <Board />
+// </div>
+// }
+console.log('hello')
+ReactDOM.render(
+    <App/>,
+document.querySelector('#App')
+)
